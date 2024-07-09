c847f1f681ac0735ccd18a22e296578abf7aed84
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-07-09 23:31:28 +0900

    footerの作成

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { notoSansJp } from "./components/util/Font";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body className={notoSansJp.className}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
