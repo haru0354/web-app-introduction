c534ee7f3cb59219098adc5c38be96595fefad2c
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-07-19 12:51:05 +0900

    色とサイズをpropsで変更できるボタンコンポーネントの作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Button from "./components/ui/Button";
 import InputText from "./components/ui/InputText";
 import Textarea from "./components/ui/Textarea";
 
@@ -5,9 +6,18 @@ export default function Home() {
   return (
     <main className="flex flex-col md:flex-row justify-between w-full max-w-[1140px] mx-auto">
       <div className="w-full max-w-[800px] px-2">
-       <h2>main</h2>
+        <h2>main</h2>
         <InputText label="テスト" name="TEST" placeholder="テスト" />
         <Textarea label="テスト" name="TEST" placeholder="テスト" />
+        <Button color={"blue"} size={"normal"}>
+          追加
+        </Button>
+        <Button color={"red"} size={"normal"}>
+          削除
+        </Button>
+        <Button color={"gray"} size={"normal"}>
+          キャンセル
+        </Button>
       </div>
       <div className="w-full max-w-[300px] px-2">side</div>
     </main>
