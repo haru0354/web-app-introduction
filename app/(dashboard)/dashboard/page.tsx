33c2fb4c85123e8d21bf3e4986b083428dbe3c59
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-07-25 00:33:34 +0900

    プロフィールコンポーネントの作成

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import Profile from "@/app/components/Profile";
+import { user } from "@/seeds/user";
+import React from "react";
 
 const page = () => {
   return (
-    <div>dashboardPage</div>
-  )
-}
+    <>
+      <h1 className="h1">プロフィール</h1>
+      <Profile
+        userName={user.name}
+        profile={user.profile}
+      />
+    </>
+  );
+};
 
-export default page
+export default page;
