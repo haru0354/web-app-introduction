77d8a84fa54ad40c08bb74bdd685e000539f1247
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-07-26 21:10:57 +0900

    個別アプリのページにアプリの製作者へのリンクを追加

--- app/(webPage)/app/[app_id]/page.tsx
+++ app/(webPage)/app/[app_id]/page.tsx
@@ -1,6 +1,7 @@
 import ImageSlider from "@/app/components/webPage/ImageSlider";
 import prisma from "@/app/lib/prisma";
 import NotFound from "@/app/not-found";
+import Link from "next/link";
 
 const page = async ({ params }: { params: { app_id: string } }) => {
   const id = params.app_id;
@@ -18,17 +19,14 @@ const page = async ({ params }: { params: { app_id: string } }) => {
   return (
     <>
       <h1 className="h1">{appData.title}の詳細</h1>
-      {appData.images[0] && (
-        <ImageSlider
-          images={appData.images}
-        />
-      )}      
-      <p>アプリの特徴：　{appData.summary}</p>
-      <p>URL：　{appData.url}</p>
-      <p>使用技術：　{appData.technology}</p>
-      <h2 className="h2">
-        「{appData.title}」で出来ること
-      </h2>
+      {appData.images[0] && <ImageSlider images={appData.images} />}
+      <p>アプリの特徴：{appData.summary}</p>
+      <p>URL：{appData.url}</p>
+      <p>使用技術：{appData.technology}</p>
+      <Link href="/user/appData.userId" className="a">
+        アプリの製作者ページへ
+      </Link>
+      <h2 className="h2">「{appData.title}」で出来ること</h2>
       <ul className="my-8 mx-12 py-2 px-4 border border-gray-400">
         {appData.can.map((can, index) => {
           return (
@@ -38,13 +36,9 @@ const page = async ({ params }: { params: { app_id: string } }) => {
           );
         })}
       </ul>
-      <h2 className="h2">
-        「{appData.title}」の概要
-      </h2>
+      <h2 className="h2">「{appData.title}」の概要</h2>
       {appData.overview}
-      <h2 className="h2">
-        「{appData.title}」がおすすめな人・解決できること
-      </h2>
+      <h2 className="h2">「{appData.title}」がおすすめな人・解決できること</h2>
       {appData.solution}
     </>
   );
