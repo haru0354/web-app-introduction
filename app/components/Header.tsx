2c0691d107f167d48e61d4689337f0d67ca0048f
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-08-27 13:27:36 +0900

    headerのメニューをログイン中と未ログインで表示の切り替え

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,18 +1,27 @@
 import Image from "next/image";
-import Modal from "./ui/Modal";
-import FormAuth from "./auth/FormAuth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
 import { signUp } from "../action/ActionAuth";
+import FormAuth from "./auth/FormAuth";
 import FormLogin from "./auth/FormLogin";
+import LogoutButton from "./auth/LogoutButton";
+import Modal from "./ui/Modal";
 
-const Header = () => {
+const Header = async () => {
+  const session = await getServerSession(authOptions);
+
   return (
     <header className="flex justify-between items-center w-full max-w-[1140px] mx-auto px-2">
       <Image src="/logo.png" width={200} height={80} alt="ロゴ" />
       <ul className="flex">
         <li className="px-4 py-2">
-          <Modal buttonText="ログイン" buttonColor="blue" buttonSize="small">
-            <FormLogin />
-          </Modal>
+          {session ? (
+            <LogoutButton />
+          ) : (
+            <Modal buttonText="ログイン" buttonColor="white" buttonSize="small">
+              <FormLogin />
+            </Modal>
+          )}
         </li>
         <li className="px-4 py-2">
           <Modal buttonText="登録" buttonColor="blue" buttonSize="small">
