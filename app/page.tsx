344090f52fe0919d28fdde68c664d902f3ee12c5
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-08-28 00:46:35 +0900

    topページを開発しやすいように一時的に各ページのリンクを貼り付け

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,14 @@
-import { addAppIntroduction } from "./action/ActionAppIntroduction";
-import { signUp } from "./action/ActionAuth";
-import FormSignUp from "./components/auth/FormSignUp";
-import FormAppIntroduction from "./components/dashboard/FormAppIntroduction";
-import Button from "./components/ui/Button";
-import InputImage from "./components/ui/InputImage";
-import InputText from "./components/ui/InputText";
-import Modal from "./components/ui/Modal";
-import Textarea from "./components/ui/Textarea";
-import NewAppIntroductions from "./components/webPage/sideMenu/NewAppIntroductions ";
-import SideLink from "./components/webPage/sideMenu/SideLink";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex flex-col md:flex-row justify-between w-full max-w-[1140px] mx-auto">
       <div className="w-full max-w-[800px] px-2">
-        <h2>main</h2>
-        <InputText label="テスト" name="TEST" placeholder="テスト" />
-        <Textarea label="テスト" name="TEST" placeholder="テスト" />
-        <Button color="blue" size="small">
-          登録
-        </Button>
-        <Button color="red" size="normal">
-          削除
-        </Button>
-        <Button color="gray" size="normal">
-          キャンセル
-        </Button>
-        <Modal buttonText="Test000" buttonColor="gray" buttonSize="normal" >
-          <FormAppIntroduction
-            formName="追加フォーム"
-            formAction={addAppIntroduction}
-          />
-        </Modal>
-        <Modal buttonText="登録" buttonColor="blue" buttonSize="small" >
-          <FormSignUp
-            formAction={signUp}
-          />
-        </Modal>
-        <NewAppIntroductions />
-        <SideLink />
-        <InputImage name={"a"} placeholder={"a"} />
+        <ul>
+          <li><Link href="/dashboard">ダッシュボード</Link></li>
+          <li><Link href="/user">登録しているユーザー一覧</Link></li>
+          <li><Link href="/app">登録されてるアプリ一覧</Link></li>
+        </ul>
       </div>
       <div className="w-full max-w-[300px] px-2">side</div>
     </main>
