7d31e00837f923a9add3a74b4202aea1a16a7e94
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-08-29 03:26:12 +0900

    セッション中のユーザー関係の取得するデータの関数名にSessionを追加して使用してるページを修正

--- app/lib/SessionUserService.ts
+++ app/lib/SessionUserService.ts
@@ -2,7 +2,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import prisma from "../lib/prisma";
 
-export const getUserData = async () => {
+export const getSessionUser = async () => {
   try {
     const session = await getServerSession(authOptions);
 
@@ -38,7 +38,7 @@ export const getUserData = async () => {
   }
 };
 
-export const getUserProfile = async () => {
+export const getSessionUserProfile = async () => {
   try {
     const session = await getServerSession(authOptions);
 
