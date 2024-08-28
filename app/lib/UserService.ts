8aa203d932cd5560d8f21ff4143dfda3548c4fef
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-08-29 02:51:54 +0900

    ユーザー一覧を返す関数を作成し、ユーザー一覧ページを変更

--- app/lib/UserService.ts
+++ app/lib/UserService.ts
@@ -2,6 +2,28 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import prisma from "../lib/prisma";
 
+export const getAllUser = async () => {
+  try {
+    const allUser = await prisma.user.findMany();
+
+    if (!allUser) {
+      return null;
+    }
+
+    const allUserData = allUser.map((user) => ({
+      id: user.id,
+      name: user.name,
+      image: user.image,
+      profile: user.profile,
+    }));
+
+    return allUserData
+  } catch (error) {
+    console.error("全てのユーザーのデータ取得中にエラーが発生しました。:", error);
+    return 
+  }
+};
+
 export const getUserData = async () => {
   try {
     const session = await getServerSession(authOptions);
