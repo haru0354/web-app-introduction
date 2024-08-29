404aff0937e045ef5ab71fb54dceaf0a3c299d7e
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-08-29 12:46:53 +0900

    アプリの詳細ページをDBからデータの取得方法に変更

--- app/lib/AppIntroductionService.ts
+++ app/lib/AppIntroductionService.ts
@@ -9,3 +9,34 @@ export const getAllAppIntroductions = async () => {
 
   return allAppIntroductions;
 };
+
+export const getAppIntroduction = async (appId: string) => {
+  const appIntroduction = await prisma.appIntroduction.findUnique({
+    where: {
+      id: appId,
+    },
+    include: {
+      user: true,
+    },
+  });
+
+  if (!appIntroduction) {
+    return null;
+  }
+
+  return {
+    userId: appIntroduction.user.id,
+    userName: appIntroduction.user.name,
+    images: appIntroduction.images,
+    id: appIntroduction.id,
+    title: appIntroduction.title,
+    summary: appIntroduction.summary,
+    url: appIntroduction.url,
+    technology: appIntroduction.technology,
+    overview: appIntroduction.overview,
+    solution: appIntroduction.solution,
+    can: appIntroduction.can,
+    createdAt: appIntroduction.createdAt,
+    updatedAt: appIntroduction.updatedAt,
+  };
+};
