ee7d125567a454dcd9aadf33e6acae5f29a45541
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-09-01 15:04:51 +0900

    signUpページの作成

--- app/components/layouts/with-children/OneColumn.tsx
+++ app/components/layouts/with-children/OneColumn.tsx
@@ -1,12 +1,12 @@
 type OneColumnProps = {
   children: React.ReactNode;
-  center: boolean;
+  center?: boolean;
 };
 
 const OneColumn: React.FC<OneColumnProps> = ({ children, center = false }) => {
   return (
-    <main className={`flex-grow w-full my-12 ${center && "flex items-center justify-center"}`}>
-      <div className="w-full max-w-[1140px] mx-auto px-2">
+    <main className={`flex-grow w-full py-12 ${center && "flex items-center justify-center bg-blue-50"}`}>
+      <div className={`w-full max-w-[1140px] mx-auto px-2 ${center && "bg-blue-50"}`}>
         {children}
       </div>
     </main>
