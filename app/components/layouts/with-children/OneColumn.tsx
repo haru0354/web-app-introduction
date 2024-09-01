1bbe72a92a480d3b660cb1ac62f9fbad1be58d60
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-09-01 14:44:52 +0900

    Onecolumnコンポーネントをpropsでコンテンツを画面中央に配置できるよう変更

--- app/components/layouts/with-children/OneColumn.tsx
+++ app/components/layouts/with-children/OneColumn.tsx
@@ -1,10 +1,11 @@
 type OneColumnProps = {
   children: React.ReactNode;
+  center: boolean;
 };
 
-const OneColumn: React.FC<OneColumnProps> = ({ children }) => {
+const OneColumn: React.FC<OneColumnProps> = ({ children, center = false }) => {
   return (
-    <main className="flex-grow w-full my-12">
+    <main className={`flex-grow w-full my-12 ${center && "flex items-center justify-center"}`}>
       <div className="w-full max-w-[1140px] mx-auto px-2">
         {children}
       </div>
