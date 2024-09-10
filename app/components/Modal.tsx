3398c2c5a92505e2fd171723ade968291a7dd963
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-09-10 14:16:02 +0900

    ModalProviderで囲ったmodalコンポーネントの作成

---
+++ app/components/Modal.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { ModalProvider } from "./context/ModalContext";
+import ModalLayout from "./layouts/with-children/ModalLayout";
+
+type ModalProps = {
+  buttonColor?: "red" | "blue" | "gray" | "white";
+  buttonText: string;
+  buttonSize?: "normal" | "small";
+  width?: string;
+  children: React.ReactNode;
+};
+
+const Modal: React.FC<ModalProps> = ({
+  buttonColor,
+  buttonText,
+  buttonSize,
+  width,
+  children,
+}) => {
+  return (
+    <ModalProvider>
+      <ModalLayout
+        buttonColor={buttonColor}
+        buttonText={buttonText}
+        buttonSize={buttonSize}
+        width={width}
+      >
+        {children}
+      </ModalLayout>
+    </ModalProvider>
+  );
+};
+
+export default Modal;
