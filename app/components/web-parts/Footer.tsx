0eb0e8a2475876c200109bf9da4fca4ad1ccef09
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-09-18 11:35:29 +0900

    フッターよりプライバシーポリシーへのリンク追加

--- app/components/web-parts/Footer.tsx
+++ app/components/web-parts/Footer.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
+
 const Footer = () => {
   return (
     <footer className="flex flex-col justify-center items-center w-full py-1 text-sm text-white bg-green-600">
       <ul className="flex">
-        <li className="py-1 mx-4">プライバシーポリシー・免責事項</li>
-        <li className="py-1">プライバシーポリシー・免責事項</li>
+        <li className="py-1 mx-4">
+          <Link href="/privacypolicy">プライバシーポリシー・免責事項</Link>
+        </li>
       </ul>
       &copy;サイトタイトル
     </footer>
