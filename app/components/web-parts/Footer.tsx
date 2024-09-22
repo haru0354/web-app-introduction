17e554e18c0113f8a57ccb71055169ca3b8a59b2
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-09-22 22:17:03 +0900

    footerのデザイン変更

--- app/components/web-parts/Footer.tsx
+++ app/components/web-parts/Footer.tsx
@@ -2,13 +2,13 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col justify-center items-center w-full py-1 text-sm text-white bg-green-600">
+    <footer className="flex flex-col justify-center items-center w-full py-1 text-sm text-white bg-gray-900">
       <ul className="flex">
-        <li className="py-1 mx-4">
+        <li className="py-1 mx-4 hover:text-customBlue">
           <Link href="/privacypolicy">プライバシーポリシー・免責事項</Link>
         </li>
       </ul>
-      &copy;サイトタイトル
+      <p>&copy;サイトタイトル</p>
     </footer>
   );
 };
