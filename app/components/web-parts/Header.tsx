8d6c0e97e25c3f20298fd5b5fe670320857dc76b
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-09-22 22:38:19 +0900

    headerのデザイン変更

--- app/components/web-parts/Header.tsx
+++ app/components/web-parts/Header.tsx
@@ -12,42 +12,44 @@ const Header = () => {
   const { data: session } = useSession();
 
   return (
-    <header className="flex justify-between items-center w-full max-w-[1140px] mx-auto px-2">
-      <Link href="/">
-        <Image src="/logo.png" width={200} height={80} alt="ロゴ" />
-      </Link>
-      <ul className="flex">
-        {session ? (
-          <>
-            <li className="px-4 py-2">
-              <LogoutButton />
-            </li>
-            <li className="px-4 py-2 my-auto">
-              <Link href="/dashboard">ダッシュボード</Link>
-            </li>
-          </>
-        ) : (
-          <>
-            <li className="px-4 py-2">
-              <Modal
-                buttonText="ログイン"
-                width="400"
-                buttonColor="white"
-                buttonSize="small"
-              >
-                <FormLogin />
-              </Modal>
-            </li>
-            <li className="px-4 py-2">
-              <Link href="/signup">
-                <Button size="small" color="blue">
-                  登録
-                </Button>
-              </Link>
-            </li>
-          </>
-        )}
-      </ul>
+    <header className="w-full bg-gray-700">
+      <div className="flex justify-between items-center w-full max-w-[1140px] mx-auto px-2">
+        <Link href="/">
+          <Image src="/logo.png" width={200} height={80} alt="ロゴ" />
+        </Link>
+        <ul className="flex">
+          {session ? (
+            <>
+              <li className="px-4 py-2">
+                <LogoutButton />
+              </li>
+              <li className="px-4 py-2 my-auto">
+                <Link href="/dashboard">ダッシュボード</Link>
+              </li>
+            </>
+          ) : (
+            <>
+              <li className="px-4 py-2">
+                <Modal
+                  buttonText="ログイン"
+                  width="400"
+                  buttonColor="white"
+                  buttonSize="small"
+                >
+                  <FormLogin />
+                </Modal>
+              </li>
+              <li className="px-4 py-2">
+                <Link href="/signup">
+                  <Button size="small" color="blue">
+                    登録
+                  </Button>
+                </Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </div>
     </header>
   );
 };
