5d33509d7933db7659a9abac4e13803eb3622970
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-11-01 18:22:55 +0900

    アプリの編集ページのmetadataの追加

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,4 +1,12 @@
+import { Metadata } from "next";
 import OneColumn from "../components/layouts/with-children/OneColumn";
+
+export const metadata: Metadata = {
+  robots: {
+    index: false,
+    follow: false,
+  },
+};
 
 export default function RootLayout({
   children,
