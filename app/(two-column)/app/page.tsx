b44a6fb12f025a776e52e726481e388396e48f72
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-11-01 18:29:22 +0900

    webアプリの一覧ページのmeta

--- app/(two-column)/app/page.tsx
+++ app/(two-column)/app/page.tsx
@@ -1,5 +1,11 @@
+import { Metadata } from "next";
 import { getAllAppIntroductions } from "@/app/lib/appIntroductionService";
 import AppsList from "@/app/components/web-parts/contents-area/AppsList";
+
+export const metadata: Metadata = {
+  title: "WEBアプリの一覧",
+  description: "このページはWEBアプリの一覧ページです。様々なWEBアプリを見つけることができるようになっています。また、自作したwebアプリを登録して表示をすることも可能となっています。便利なアプリを探している方はご覧ください。",
+};
 
 const page = async () => {
   const allAppIntroductions = await getAllAppIntroductions();
