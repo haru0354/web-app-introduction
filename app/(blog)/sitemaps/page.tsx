06f7f4a35d0e1b4aafe2932128afa8738483bd21
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-11-03 11:53:36 +0900

    サイトマップページの作成

---
+++ app/(blog)/sitemaps/page.tsx
@@ -0,0 +1,51 @@
+import Link from "next/link";
+import { Metadata } from "next";
+import { getAllAppIntroductions } from "@/app/lib/appIntroductionService";
+import BlogTopInfo from "@/app/components/layouts/BlogTopInfo";
+
+export const metadata: Metadata = {
+  title: "サイトマップ",
+  description:
+    "このページではサイトマップとなります。当サイトの全ページへの動線が用意されており、該当のページへすぐにアクセスすることができます。",
+};
+
+const page = async () => {
+  const allAppIntroductions = await getAllAppIntroductions();
+
+  if (!allAppIntroductions) {
+    return null;
+  }
+
+  return (
+    <>
+      <BlogTopInfo title="サイトマップ" date="2024-11-03" image={false} />
+      <ul className="list-disc list-inside mx-4">
+        <li className="py-2">
+          <Link href="/app">WEBアプリ一覧ページ</Link>
+        </li>
+        <ul className="list-disc list-inside mx-6">
+          {allAppIntroductions.map((allAppIntroduction) => (
+            <li key={allAppIntroduction.id} className="py-2">
+              <Link href={`/app/${allAppIntroduction.id}`}>
+                {allAppIntroduction.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <li className="py-2">
+          <Link href="/user">ユーザー一覧ページ</Link>
+        </li>
+      </ul>
+      <ul className="list-disc list-inside mx-4">
+        <li className="py-2">
+          <Link href="/signup">アカウント登録ページ</Link>
+        </li>
+        <li className="py-2">
+          <Link href="/privacypolicy">プライバシーポリシー</Link>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default page;
