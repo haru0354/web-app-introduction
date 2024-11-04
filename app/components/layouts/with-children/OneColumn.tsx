4616ac0bdc241f0a39e7cc2cdbb3d312312ed580
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-11-04 17:09:54 +0900

    oneカラムとtwoカラムのlayoutにパンくずリストを追加

--- app/components/layouts/with-children/OneColumn.tsx
+++ app/components/layouts/with-children/OneColumn.tsx
@@ -1,3 +1,5 @@
+import Breadcrumbs from "../../web-parts/Breadcrumbs";
+
 type OneColumnProps = {
   children: React.ReactNode;
   center?: boolean;
@@ -5,8 +7,15 @@ type OneColumnProps = {
 
 const OneColumn: React.FC<OneColumnProps> = ({ children, center = false }) => {
   return (
-    <main className={`flex-grow w-full p-2 md:py-12 bg-layout-bgColor ${center && "flex items-center justify-center"}`}>
-      <div className={`w-full max-w-[1140px] mx-auto py-1 px-4 md:px-6 bg-white rounded`}>
+    <main
+      className={`flex-grow w-full p-2 md:py-12 bg-layout-bgColor ${
+        center && "flex items-center justify-center"
+      }`}
+    >
+      <Breadcrumbs />
+      <div
+        className={`w-full max-w-[1140px] mx-auto py-1 px-4 md:px-6 bg-white rounded`}
+      >
         {children}
       </div>
     </main>
--- app/components/layouts/with-children/TwoColumn.tsx
+++ app/components/layouts/with-children/TwoColumn.tsx
@@ -1,3 +1,4 @@
+import Breadcrumbs from "../../web-parts/Breadcrumbs";
 import SideMenu from "../../web-parts/side-menu/SideMenu";
 
 type TwoColumnProps = {
@@ -11,8 +12,13 @@ const TwoColumn: React.FC<TwoColumnProps> = ({
 }) => {
   return (
     <main className="w-full p-2 md:py-12 bg-layout-bgColor">
+      <Breadcrumbs />
       <div className="flex-grow flex flex-col md:flex-row justify-between max-w-[1140px] mx-auto bg-white rounded">
-        <div className={`w-full max-w-[800px] px-4 md:px-6 ${blogPage ? "blog" : ""}`}>
+        <div
+          className={`w-full max-w-[800px] px-4 md:px-6 ${
+            blogPage ? "blog" : ""
+          }`}
+        >
           {children}
         </div>
         <SideMenu />
