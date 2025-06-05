857e7b7a1d7fd996726c6eaf71d23b2f94e0c5b3
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-06-05 13:12:31 +0900

    ユーザーの一覧をgridデザインに変更

--- app/(two-column)/user/page.tsx
+++ app/(two-column)/user/page.tsx
@@ -6,7 +6,8 @@ import { getAllUser } from "@/app/lib/service/userService";
 
 export const metadata: Metadata = {
   title: "ユーザーの一覧",
-  description: "このページはWEBアプリを登録しているユーザーの一覧ページです。様々なWEBアプリの製作者を見つけることができるようになっています。",
+  description:
+    "このページはWEBアプリを登録しているユーザーの一覧ページです。様々なWEBアプリの製作者を見つけることができるようになっています。",
   robots: {
     index: false,
   },
@@ -21,26 +22,23 @@ const page = async () => {
 
   return (
     <>
-      <h1 className="h1">WEBアプリを登録しているユーザーの一覧</h1>
-      <div className="flex flex-wrap w-full">
+      <h1 className="h1">ユーザーの一覧</h1>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 w-full">
         {allUser.map((user) => (
-          <div
-            key={user.id}
-            className="flex flex-col items-center justify-center w-full max-w-[120px] text-center mx-4"
-          >
-            <Link href={`/user/${user.id}`}>
+          <Link href={`/user/${user.id}`} key={user.id}>
+            <div className="flex flex-col items-center justify-center w-full max-w-[120px] text-center mx-4 hover:-translate-y-2 transition">
               <Image
                 src={user.image ? user.image : "/test.jpg"}
                 width={120}
                 height={120}
                 alt="ユーザーの画像"
-                className="rounded-full hover:-translate-y-2 transition"
+                className="rounded-full "
               />
-            </Link>
-            <p>「{user.name ? user.name : "名無し"}」</p>
-            {user.profile?.occupation && <p>{user.profile.occupation}</p>}
-            {user.profile?.skill && <p>{user.profile.skill}</p>}
-          </div>
+              <p>「{user.name ? user.name : "名無し"}」</p>
+              {user.profile?.occupation && <p>{user.profile.occupation}</p>}
+              {user.profile?.skill && <p>{user.profile.skill}</p>}
+            </div>
+          </Link>
         ))}
       </div>
     </>
