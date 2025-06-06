f2aff568b32f6953828738e40fd272af5bd5253a
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-06-06 13:39:19 +0900

    アカウント登録ページでログイン中ユーザーはダッシュボードにリダイレクト

--- app/(center-contents)/signup/page.tsx
+++ app/(center-contents)/signup/page.tsx
@@ -1,5 +1,7 @@
 import { Metadata } from "next";
+import { redirect } from "next/navigation";
 
+import { getSessionUserId } from "@/app/lib/service/sessionUserService";
 import { signUp } from "@/app/action/actionUser";
 import FormSignUp from "@/app/components/form/FormSignUp";
 
@@ -11,7 +13,13 @@ export const metadata: Metadata = {
   },
 };
 
-const page = () => {
+const page = async () => {
+  const userId = await getSessionUserId()
+
+  if (userId) {
+    redirect("/dashboard");
+  }
+
   return (
     <div className="w-full max-w-[400px] p-4 mx-auto border rounded border-gray-400 bg-white">
       <FormSignUp formAction={signUp} />
