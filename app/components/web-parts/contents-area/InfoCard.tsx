6b4f631a8e471403bd0d9ef94a4b7a3fc5edb2ef
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-06-06 11:22:11 +0900

    InfoCardのデザイン変更

--- app/components/web-parts/contents-area/InfoCard.tsx
+++ app/components/web-parts/contents-area/InfoCard.tsx
@@ -17,7 +17,7 @@ const InfoCard: React.FC<InfoCardProps> = ({
   alt,
 }) => {
   const cardContent = (
-    <div className="flex flex-col md:flex-row items-center justify-center w-full p-6 border rounded bg-gray-600">
+    <div className="flex flex-col md:flex-row items-center justify-center w-full p-6 border rounded shadow-xl bg-gray-600">
       <div className="w-full md:w-[70%] my-auto md:pr-8">
         <p className="text-white text-2xl md:text-3xl mt-2 mb-8">{title}</p>
         <p className="text-white md:text-lg">{content}</p>
